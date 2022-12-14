8617bc498a5ec73323464cb79f851423a907f5b4
Samuel Attwood <45669855+samuelattwood@users.noreply.github.com>
2022-12-13 20:44:25 -0500

    Adding deployment instructions and config (#1)
    * Adding initial config
    
    * Missed prometheus.yml
    
    * Fixing operator key issue in bootstrap.sh. Updating image. Temporarily removing persistent volume for rna due to permissions issue.
    
    * Updating bootstrap script to support alpine's busybox /bin/sh. Adding nats-box setup container to docker-compose. Adding gitignore
    
    * Removing bootstrap step from README
    
    * Updating default behavior for setup to keep existing config directory
    
    * Updating README
    
    * prometheus is embedded now
    
    * update instructions
    
    Signed-off-by: Caleb Lloyd <caleb@synadia.com>
    
    Signed-off-by: Caleb Lloyd <caleb@synadia.com>
    Co-authored-by: Samuel Attwood <sam@attwood.io>
    Co-authored-by: Caleb Lloyd <2414837+caleblloyd@users.noreply.github.com>
    Co-authored-by: Caleb Lloyd <caleb@synadia.com>

---
+++ bootstrap.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+cd "$(dirname "${0}")"
+
+CLUSTERS="nats-a nats-b nats-c"
+CONFIG_DIR="$(pwd)/nats"
+NSC_DIR=${CONFIG_DIR}/nsc
+NSC_FLAGS="--config-dir=${NSC_DIR} --data-dir=${NSC_DIR}/store --keystore-dir=${NSC_DIR}/keys"
+NATS_SERVER_CONFIG=$(cat <<EOF
+port: 4222
+
+jetstream {
+  store_dir: /data
+  max_mem: 0
+  max_file: 10GB
+}
+EOF
+)
+
+RNA_CONFIG=$(cat <<EOF
+{
+  env: "local"
+  postgres_url: "postgres://helix:helix@localhost:5432/helix"
+  prometheus_url: "http://localhost:9090"
+  logging: {
+    components: {
+      auth: {
+        level: "warn"
+      }
+    }
+  }
+EOF
+)
+
+if [[ -d ${CONFIG_DIR} ]] && [[ "${1}" == "-s" ]]; then
+  echo "Using existing config directory"
+  exit 0
+elif [[ -d ${CONFIG_DIR} ]]; then
+  if [[ "${1}" != "-f" ]]; then
+    echo "Config directory exists"
+    read -p "Would you like to delete and replace it? (y/N) "
+    if ! echo ${REPLY} | grep -q '^[Yy]'; then
+      exit 1
+    fi
+  fi
+  rm -rf ${CONFIG_DIR}
+fi
+
+mkdir -p ${CONFIG_DIR}
+
+NATS_SYSTEMS="\n  nats_systems: ["
+for cluster in ${CLUSTERS}; do
+  # Create cluster operator
+  nsc ${NSC_FLAGS} add operator ${cluster}
+  nsc ${NSC_FLAGS} edit operator --service-url nats://${cluster}:4222
+
+  # Add system account
+  nsc ${NSC_FLAGS} add account -n SYS
+  nsc ${NSC_FLAGS} edit operator --system-account SYS
+
+  # Add system user
+  nsc ${NSC_FLAGS} add user -a SYS -n sys
+
+  # Create and associate operator signing key
+  OPERATOR_NKEY_OUT=$(nsc ${NSC_FLAGS} generate nkey --operator --store 2>&1)
+  OPERATOR_KEY=$(echo "${OPERATOR_NKEY_OUT}" |head -1)
+  OPERATOR_KEY_PATH=$(echo "${OPERATOR_NKEY_OUT}" |grep '.nk$' |awk '{ print $4 }' |sed "s%^$(pwd)/nats/%%")
+  nsc ${NSC_FLAGS} edit operator --sk ${OPERATOR_KEY}
+  chmod +r ${CONFIG_DIR}/${OPERATOR_KEY_PATH}
+  chmod +r ${CONFIG_DIR}/nsc/keys/creds/${cluster}/SYS/sys.creds
+
+  # Create nats server config
+  mkdir -p ${CONFIG_DIR}/${cluster}
+  NATS_CFG=${CONFIG_DIR}/${cluster}/nats-server.conf
+  echo "${NATS_SERVER_CONFIG}" > ${NATS_CFG}
+  nsc ${NSC_FLAGS} generate config --nats-resolver >> ${NATS_CFG}
+
+  # Update resolver jwt directory to use /data
+  sed -i "s%\(dir: \)'./jwt'%\1'/data/jwt'%" ${NATS_CFG}
+
+  # Update resolver to allow jwt deletion
+  sed -i 's%\(allow_delete: \)false%\1true%' ${NATS_CFG}
+
+  # Setup nats server config for RNA
+  NATS_SYSTEM=$(cat <<EOF
+    {
+      name:                      "${cluster}"
+      urls:                      "nats://${cluster}:4222"
+      account_server_url:        "nats://${cluster}:4222"
+      system_account_creds_file: "/etc/nsc/keys/creds/${cluster}/SYS/sys.creds"
+      operator_signing_key_file: "/etc/${OPERATOR_KEY_PATH}"
+    },
+EOF
+  )
+  # Append nats server config to array
+  NATS_SYSTEMS="${NATS_SYSTEMS}\n${NATS_SYSTEM}"
+done
+
+# Append nats system array to RNA config
+RNA_CONFIG="${RNA_CONFIG}${NATS_SYSTEMS::-1}]\n}"
+
+# Write out RNA config to file
+echo -e "${RNA_CONFIG}" > rna.cue
+
+# Ensure directory tree is globally navigable
+find ${CONFIG_DIR} -type d -exec chmod 755 {} \;
